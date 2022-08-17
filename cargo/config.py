2ac7cd8704f49adacad6f2088ff5ce73b53760de
FlaShHolloway <46559175+FlaShHolloway@users.noreply.github.com>
2022-08-17 13:43:55 +0530

    Update config.py

--- cargo/config.py
+++ cargo/config.py
@@ -3,7 +3,7 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 import os
 
 SECRET_KEY = 'THISISKYEY'
-DATABASE = r"root:root@127.0.0.1/"
+DATABASE = r"admin:613Jio613@cargoreginfo.c9je3iddxfzn.ap-south-1.rds.amazonaws.com/"
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + DATABASE + 'jobdone'
 SERVER_URL = 'http://cargo.win'
 STEAM_API_KEY = '4D68245A82B5ACC74BD1068378A09AAD'
