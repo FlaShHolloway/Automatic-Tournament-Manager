5702702039348505e864c57454d53e01a12a6ba6
FlaShHolloway <46559175+FlaShHolloway@users.noreply.github.com>
2022-08-18 13:03:35 +0530

    Update config.py

--- cargo/config.py
+++ cargo/config.py
@@ -3,8 +3,8 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 import os
 
 SECRET_KEY = 'THISISKYEY'
-DATABASE = r"admin:613Jio613@cargoreginfo.c9je3iddxfzn.ap-south-1.rds.amazonaws.com/"
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + DATABASE + 'jobdone'
+DATABASE = r"/root/cargowin/"
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE + 'database.db&apos'
 SERVER_URL = 'cargowin.herokuapp.com'
 STEAM_API_KEY = '4D68245A82B5ACC74BD1068378A09AAD'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
@@ -12,7 +12,7 @@ RECAPTCHA_USE_SSL = False
 RECAPTCHA_PUBLIC_KEY = '6LctrIMhAAAAAKPG3dqPPLsiNPobQCJeD_JtpsV-'
 RECAPTCHA_PRIVATE_KEY = '6LctrIMhAAAAAFcOhIVOeOwVpTvN-P6FGZCuGmQo'
 RECAPTCHA_OPTIONS = {'theme': 'dark'}
-SCHEDULER_JOBSTORES = {"default": SQLAlchemyJobStore(url="mysql+pymysql://" + os.path.join(DATABASE, 'jobstore'))}
+SCHEDULER_JOBSTORES = {"default": SQLAlchemyJobStore(url="sqlite:///" + os.path.join(DATABASE, 'jobstore.db&apos'))}
 SCHEDULER_EXECUTORS = {"default": {"type": "threadpool", "max_workers": 20}}
 SCHEDULER_JOB_DEFAULTS = {"coalesce": False, "max_instances": 3}
 SCHEDULER_API_ENABLED = False
